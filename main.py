b6e1f20449eabff18ac6900323a3f062c6c508b4
Ben Frey <spartanhooah@gmail.com>
2022-12-18 10:47:44 -0500

    improve email body

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import os
 from send_email import send_email
 
 api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?language=en&apiKey={api_key}"
 
 content = requests.get(url).json()
 
-message = f"""\
-Subject: News digest
+message = "Subject: News digest\n"
 
-"""
-
-for article in content["articles"]:
+for article in content["articles"][:20]:
     message = message + "\n"
+
     if article["title"]:
         message = message + article["title"] + "\n"
 
     if article["description"]:
         message = message + article["description"] + "\n"
+
+    if article["url"]:
+        message = message + article["url"] + "\n"
 
 send_email(message)
