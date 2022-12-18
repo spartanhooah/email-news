4908f8b2c75fad8d22b104f3f31cdafaa12b0fa3
Ben Frey <spartanhooah@gmail.com>
2022-12-18 12:45:07 -0500

    use pagination on the URL

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import os
 from send_email import send_email
 
 api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/top-headlines?language=en&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?language=en&pageSize=20&apiKey={api_key}"
 
 content = requests.get(url).json()
 
 message = "Subject: News digest\n"
 
-for article in content["articles"][:20]:
+for article in content["articles"]:
     message = message + "\n"
 
     if article["title"]:
